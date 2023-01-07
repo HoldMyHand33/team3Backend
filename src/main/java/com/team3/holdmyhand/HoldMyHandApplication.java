850ac933eadfaaf8d38c36d0426e89041ba9ef91
jmk307 <jimin112688@gmail.com>
2023-01-07 16:25:44 +0900

    Update HoldMyHandApplication.java

--- src/main/java/com/team3/holdmyhand/HoldMyHandApplication.java
+++ src/main/java/com/team3/holdmyhand/HoldMyHandApplication.java
@@ -2,9 +2,16 @@ package com.team3.holdmyhand;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class HoldMyHandApplication {
+public class HoldMyHandApplication extends SpringBootServletInitializer {
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+		return builder.sources(HoldMyHandApplication.class);
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(HoldMyHandApplication.class, args);
