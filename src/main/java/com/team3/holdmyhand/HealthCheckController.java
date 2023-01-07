0fdd5d91ae2d89bbf90ed373347674e3af6249e8
jmk307 <jimin112688@gmail.com>
2023-01-07 16:55:08 +0900

    feat : commonApiResponse

--- src/main/java/com/team3/holdmyhand/HealthCheckController.java
+++ src/main/java/com/team3/holdmyhand/HealthCheckController.java
@@ -1,6 +1,6 @@
 package com.team3.holdmyhand;
 
-import com.team3.holdmyhand.global.config.CommonApiResponse;
+import com.team3.holdmyhand.global.CommonApiResponse;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
