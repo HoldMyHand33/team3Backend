9f6db0c9bc4265e24c3cce02ad8e18f4d8d23316
jmk307 <jimin112688@gmail.com>
2023-01-07 18:06:45 +0900

    Update LoginDto.java

--- src/main/java/com/team3/holdmyhand/domain/member/dto/LoginDto.java
+++ src/main/java/com/team3/holdmyhand/domain/member/dto/LoginDto.java
@@ -1,5 +1,6 @@
 package com.team3.holdmyhand.domain.member.dto;
 
+import io.swagger.annotations.ApiModelProperty;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -9,6 +10,8 @@ import lombok.Setter;
 @AllArgsConstructor
 @NoArgsConstructor
 public class LoginDto {
+    @ApiModelProperty(example = "jimin112688@gmail.com")
     private String email;
+    @ApiModelProperty(example = "rejin0421")
     private String password;
 }
