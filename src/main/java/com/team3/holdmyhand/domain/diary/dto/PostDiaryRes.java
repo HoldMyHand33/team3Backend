605539c989ac9a9b738d5d4628dcea7e2355d512
JjungminLee <julie0964@naver.com>
2023-01-07 23:04:30 +0900

    feat:feature/diary 추가

--- src/main/java/com/team3/holdmyhand/domain/diary/dto/PostDiaryRes.java
+++ src/main/java/com/team3/holdmyhand/domain/diary/dto/PostDiaryRes.java
@@ -1,7 +1,11 @@
 package com.team3.holdmyhand.domain.diary.dto;
 
+import com.team3.holdmyhand.domain.diary.entity.PostDiary;
+import com.team3.holdmyhand.domain.member.entity.Member;
 import lombok.Builder;
 import lombok.Data;
+
+import java.util.List;
 
 @Data
 @Builder
@@ -9,5 +13,7 @@ import lombok.Data;
 public class PostDiaryRes {
 
     private Long diaryId;
-    //private Long memberId;
+    private List<PostDiary>postDiaryList;
+
+
 }
