9fc7d73cf9974de64002888ac372986c92d5cc5c
jmk307 <jimin112688@gmail.com>
2023-01-07 18:30:25 +0900

    Update HealthCheckController.java

--- src/main/java/com/team3/holdmyhand/HealthCheckController.java
+++ src/main/java/com/team3/holdmyhand/HealthCheckController.java
@@ -1,18 +1,17 @@
 package com.team3.holdmyhand;
 
-import org.springframework.http.ResponseEntity;
+import com.team3.holdmyhand.global.config.CommonApiResponse;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+@Slf4j
 @RestController
-@RequestMapping("test")
+@RequiredArgsConstructor
 public class HealthCheckController {
-
     @GetMapping
-    public ResponseEntity<String> test() {
-        System.out.println("성공1234");
-        return ResponseEntity.ok("테스트1234");
+    public CommonApiResponse<Boolean> healthCheck() {
+        return CommonApiResponse.of(true);
     }
 }
-
