234885e54d805556d9c2e2eaf97aa1df208ba516
jmk307 <jimin112688@gmail.com>
2023-01-07 16:39:32 +0900

    feat : healthcheck

--- src/main/java/com/team3/holdmyhand/HealthCheckController.java
+++ src/main/java/com/team3/holdmyhand/HealthCheckController.java
@@ -1,15 +1,17 @@
 package com.team3.holdmyhand;
 
-import com.team3.holdmyhand.global.config.CommonApiResponse;
-import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequiredArgsConstructor
+@RequestMapping("test")
 public class HealthCheckController {
+
     @GetMapping
-    public CommonApiResponse<Boolean> healthCheck() {
-        return CommonApiResponse.of(true);
+    public ResponseEntity<String> test() {
+        return ResponseEntity.ok("테스트1234");
     }
 }
+
