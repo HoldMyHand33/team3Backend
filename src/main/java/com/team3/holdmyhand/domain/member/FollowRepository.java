1cb49f5f3215744dee934e2ea13b63b61013d352
jmk307 <jimin112688@gmail.com>
2023-01-08 03:45:46 +0900

    feat : 친구목록

---
+++ src/main/java/com/team3/holdmyhand/domain/member/FollowRepository.java
@@ -0,0 +1,7 @@
+package com.team3.holdmyhand.domain.member;
+
+import com.team3.holdmyhand.domain.member.entity.Follow;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface FollowRepository extends JpaRepository<Follow, Long> {
+}
--- src/main/java/com/team3/holdmyhand/domain/member/MemberService.java
+++ src/main/java/com/team3/holdmyhand/domain/member/MemberService.java
@@ -1,9 +1,7 @@
 package com.team3.holdmyhand.domain.member;
 
-import com.team3.holdmyhand.domain.member.dto.LoginDto;
-import com.team3.holdmyhand.domain.member.dto.MemberRequestDto;
-import com.team3.holdmyhand.domain.member.dto.MemberResponseDto;
-import com.team3.holdmyhand.domain.member.dto.ReconciliationRequestDto;
+import com.team3.holdmyhand.domain.member.dto.*;
+import com.team3.holdmyhand.domain.member.entity.Friend;
 import com.team3.holdmyhand.domain.member.entity.Member;
 import com.team3.holdmyhand.global.CommonApiResponse;
 import com.team3.holdmyhand.global.config.security.dto.TokenResponseDto;
@@ -19,7 +17,9 @@ import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.text.ParseException;
+import java.util.List;
 import java.util.Optional;
+import java.util.stream.Collectors;
 
 @Service
 @RequiredArgsConstructor
@@ -75,12 +75,34 @@ public class MemberService {
         return MemberResponseDto.of(member, reconciliationRequestDto);
     }
 
-    // 홈화면
-    @Transactional
-    MemberResponseDto showReconciliationDate(String email) throws ParseException {
+    // 홈 화면
+    @Transactional(readOnly = true)
+    public MemberResponseDto showReconciliationDate(String email) throws ParseException {
         Member member = memberRepository.findByEmail(email)
                 .orElseThrow(() -> new BadRequestException(ErrorCode.MEMBER_NOT_FOUND));
 
         return MemberResponseDto.of(member);
+    }
+
+    // 코드로 친구추가
+    @Transactional
+    public MemberResponseDto makeFriend(String email, CodeRequest codeRequest) throws ParseException {
+        // email 부모 code 자식
+        Member member = memberRepository.findByEmail(email)
+                .orElseThrow(() -> new BadRequestException(ErrorCode.MEMBER_NOT_FOUND));
+        Member codeMember = memberRepository.findByCode(codeRequest.getCode());
+        codeMember.updateParent(member);
+        memberRepository.save(codeMember);
+        return MemberResponseDto.of(member);
+    }
+
+    // 친구 보기
+    @Transactional(readOnly = true)
+    public List<MemberResponseDto> showFriends(String email) {
+        Member member = memberRepository.findByEmail(email)
+                .orElseThrow(() -> new BadRequestException(ErrorCode.MEMBER_NOT_FOUND));
+        return member.getMemberList().stream()
+                .map(MemberResponseDto::of)
+                .collect(Collectors.toList());
     }
 }
