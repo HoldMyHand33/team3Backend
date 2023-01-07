e77912bbf46641db4e80b8237ff6ba5f225834a4
jmk307 <jimin112688@gmail.com>
2023-01-07 15:13:05 +0900

    feat : 초기설정

---
+++ src/main/java/com/team3/holdmyhand/HoldMyHandApplication.java
@@ -0,0 +1,13 @@
+package com.team3.holdmyhand;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HoldMyHandApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HoldMyHandApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/team3/holdmyhand/HoldMyHandApplicationTests.java
@@ -0,0 +1,13 @@
+package com.team3.holdmyhand;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HoldMyHandApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
